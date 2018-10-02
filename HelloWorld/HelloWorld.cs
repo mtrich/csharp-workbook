770f2bdcfe9279dcb096e3cd346f049ebcc8f701
Michael Rich <mtrich@satx.rr.com>
2018-10-02 05:03:50 -0500

    reviewed homework code and changed code into a mad lib

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -1,12 +1,58 @@
 ﻿using System;
 
-namespace HelloWorld
+
+public class Program
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World! It's Michael Rich!");
-        }
-    }
+        
+        string adjective1 = "";
+        string adjective2 = "";
+        string adjective3 = "";
+        string adjective4 = "";
+        string adjective5 = "";
+        string adverb = "";
+        string number = "";
+        string pluralNoun1 = "";
+        string pluralNoun2 = "";
+        string pluralNoun3 = "";
+        string pluralNoun4 = "";
+        string pluralNoun5 = "";
+        string relative = "";
+        string verb1 = "";
+        string verb2 = "";
+        
+        Console.WriteLine("Enter a verb: ");
+        verb1 = Console.ReadLine();
+        Console.WriteLine("Enter an adjective: ");
+        adjective1 = Console.ReadLine();
+        Console.WriteLine("Enter a plural Noun: ");
+        pluralNoun1 = Console.ReadLine();
+        Console.WriteLine("Enter an adjective: ");
+        adjective2 = Console.ReadLine();
+        Console.WriteLine("Enter an adverb: ");
+        adverb = Console.ReadLine();
+        Console.WriteLine("Enter a verb: ");
+        verb2 = Console.ReadLine();
+        Console.WriteLine("Enter a number: ");
+        number = Console.ReadLine();
+        Console.WriteLine("Enter an adjective: ");
+        adjective3 = Console.ReadLine();
+        Console.WriteLine("Enter a plural Noun: ");
+        pluralNoun2 = Console.ReadLine();
+        Console.WriteLine("Enter a plural Noun: ");
+        pluralNoun3 = Console.ReadLine();
+        Console.WriteLine("Enter a plural Noun: ");
+        pluralNoun4 = Console.ReadLine();
+        Console.WriteLine("Enter a type of plural relative: ");
+        relative = Console.ReadLine();
+        Console.WriteLine("Enter an adjective: ");
+        adjective4 = Console.ReadLine();
+        Console.WriteLine("Enter an adjective: ");
+        adjective5 = Console.ReadLine();
+        Console.WriteLine("Enter a plural Noun: ");
+        pluralNoun5 = Console.ReadLine();
+
+        Console.WriteLine("Come {0} at WALMART, where you`ll receive {1} discounts on all of your favorite brand name {2}. Our {3} and {4} associates are there to {5} you {6} hours a day. Here you will find {7} prices on the {8} you need. {9} for the moms, {10} for the kids and all the latest electronics for the {11}. So come on down to your {12} {13} WALMART where the {14} come first.", verb1, adjective1, pluralNoun1, adjective2, adverb, verb2, number, adjective3, pluralNoun2, pluralNoun3, pluralNoun4, relative, adjective4, adjective5, pluralNoun5);
+    }   
 }
