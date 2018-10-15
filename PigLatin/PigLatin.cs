939188ae5e4debc19da7e6ed325b6e86243a23db
Michael Rich <mtrich@satx.rr.com>
2018-10-15 18:36:25 -0500

    Program translates most words to pig latin but words that dont contain a normal vowel break the code ex. my, tsktsk, cry, fly. Also need to add comments

--- PigLatin/PigLatin.cs
+++ PigLatin/PigLatin.cs
@@ -1,21 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PigLatin
 {
     class Program
     {
-        public static void Main()
-        {
-            // your code goes here
-
-            // leave this command at the end so your program does not close automatically
-            Console.ReadLine();
+        public static void Main(string[] args)
+        {    
+        Console.WriteLine("Enter a sentence to convert to PigLatin:");
+        string sentence = Console.ReadLine();
+        string pigLatin = ToPigLatin(sentence);
+        Console.WriteLine(pigLatin.ToLower());
         }
-        
-        public static string TranslateWord(string word)
-        {
-            // your code goes here
-            return word;
+
+        static string ToPigLatin (string sentence)
+        {                        
+            List<string> latin = new List<string>();
+
+            //splits user entered input
+            foreach (string word in sentence.Split(' '))
+            {
+                int firstVowelIndex = word.IndexOfAny(new char[] {'A','E','I','O','U','a','e','i','o','u',});
+                int firstYIndex = word.IndexOfAny(new char[] {'Y','y'});
+                string first = word.Substring(0, firstVowelIndex);
+                string rest = word.Substring(firstVowelIndex);
+
+                if ((firstYIndex > 0) && (firstYIndex < firstVowelIndex) && (word.ToLower().Contains("y")))
+                {
+                    string firstY = word.Substring(0, firstYIndex);
+                    string restY = word.Substring(firstYIndex);                    
+                    latin.Add(restY + firstY + "ay");
+                }
+                else if (firstVowelIndex > 0)
+                {
+                    latin.Add(rest + first + "ay");
+                }
+                else
+                {
+                    latin.Add(word + "yay");                        
+                }                   
+            }        
+            return string.Join(" ", latin);        
         }
     }
 }
